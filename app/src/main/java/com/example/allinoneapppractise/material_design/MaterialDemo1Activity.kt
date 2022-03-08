6e6dee2c780d74235a4784331733ae322e3e7f6c
Arun Purohit <rajpurohitarun97@gmail.com>
2022-03-08 23:53:11 +0530

    material theme practice

---
+++ app/src/main/java/com/example/allinoneapppractise/material_design/MaterialDemo1Activity.kt
@@ -0,0 +1,12 @@
+package com.example.allinoneapppractise.material_design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.allinoneapppractise.R
+
+class MaterialDemo1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_material_demo1)
+    }
+}
